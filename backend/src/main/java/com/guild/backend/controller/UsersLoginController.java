423e96aa92592e8039995380afbff395b0e6acf8
vlad <vasilegin@gmail.com>
2022-06-07 18:37:22 +0300

    Fix

---
+++ backend/src/main/java/com/guild/backend/controller/UsersLoginController.java
@@ -0,0 +1,36 @@
+package com.guild.backend.controller;
+
+import com.guild.backend.service.SecurityService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersLoginController {
+
+    @Autowired
+    private SecurityService securityService;
+
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (securityService.isAuthenticated()) {
+            return "redirect:/";
+        }
+
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
