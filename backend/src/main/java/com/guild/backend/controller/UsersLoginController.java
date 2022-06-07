92f7ceb9d5d02ba6fff7b18917ffcb852907594c
vlad <vasilegin@gmail.com>
2022-06-08 00:12:37 +0300

    Added profile

--- backend/src/main/java/com/guild/backend/controller/UsersLoginController.java
+++ backend/src/main/java/com/guild/backend/controller/UsersLoginController.java
@@ -1,5 +1,7 @@
 package com.guild.backend.controller;
 
+import com.guild.backend.entity.Job;
+import com.guild.backend.repo.JobRepo;
 import com.guild.backend.service.SecurityService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.guild.backend.entity.User;
 import com.guild.backend.repo.UserRepo;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UsersLoginController {
@@ -20,27 +26,41 @@ public class UsersLoginController {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private JobRepo jobRepo;
+
+
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
-        if (securityService.isAuthenticated()) {
-            return "redirect:/";
+        if (error != null) {
+            model.addAttribute("error", "Your username and password is invalid.");
+            return "login";
         }
 
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
-
-        if (logout != null)
+        if (logout != null){
             model.addAttribute("message", "You have been logged out successfully.");
+            return "login";
+        }
+
+        if (securityService.isAuthenticated()) {
+            return "redirect:/profile";
+        }
 
         return "login";
     }
+
+
 
     @GetMapping("/profile")
     public String welcome(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userRepo.findByLogin(auth.getName());
         System.out.println(user);
+//        List<Job> jobs = jobRepo.findByAdventurer_idAndDate_acceptedIsNotNull(user.getId());
+//        System.out.println(jobs);
+        List<Integer> date = List.of(1,2,3,4,5,6,7,8,9,10,11,12);
+        model.addAttribute("data", date);
         model.addAttribute("user", user);
-        return "profile";
+        return "profileDemo";
     }
 }
--- backend/src/main/java/com/guild/backend/repo/JobRepo.java
+++ backend/src/main/java/com/guild/backend/repo/JobRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guild.backend.entity.Job;
 
+import java.util.List;
+
 public interface JobRepo extends JpaRepository<Job, Long> {
+//    List<Job> findByAdventurer_idAndDate_acceptedIsNotNull(Long id);
 
 }
